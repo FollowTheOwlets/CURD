1e90478c33d1950cce27d74566801afed24fa465
Lev <fedorichev16@gmail.com>
2022-12-05 00:05:34 +0300

    di-java update

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -7,7 +7,6 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-@Repository
 public class PostRepository {
     ConcurrentMap<Long, Post> posts;
 
